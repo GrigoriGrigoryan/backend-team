77a66320bfa6b466957fb1b22ded1e6d78f1d872
Grigori Grigoryan <grigori_grigoryan@epam.com>
2021-12-14 16:57:33 +0400

    changed ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -12,6 +12,7 @@ const config: ConnectionOptions = {
 	database: process.env.POSTGRES_DB || "pg_data",
 	entities: [LeafInfo],
 	//ssl:{rejectUnauthorized:false},
+
 	migrations: [],
 	cli: {},
 	logging: true,
